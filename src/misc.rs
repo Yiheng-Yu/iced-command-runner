0f70a431db04b01b7e73728d0b01344a3e382af1
Yiheng-Yu <yuyiheng1994@gmail.com>
2026-07-05 22:34:09 +0100

    umm

--- src/misc.rs
+++ src/misc.rs
@@ -55,9 +55,6 @@ where
             button::primary(theme, button::Status::Disabled)
             .with_background(Background::Color(iced::Color::TRANSPARENT))
         )
-        .padding(0.0)
-        .width(iced::Length::Fill)
-        .height(iced::Length::Fill)
     } else {
         let msg = on_press(Event::ClearBuffer);
         button(content)
@@ -67,9 +64,6 @@ where
             button::primary(theme, status)
             .with_background(Background::Color(iced::Color::TRANSPARENT))
         )
-        .padding(0.0)
-        .width(iced::Length::Fill)
-        .height(iced::Length::Fill)
     }
 }
 
